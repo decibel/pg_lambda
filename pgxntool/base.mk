205d0c97ae252553ec538aa38d084dd23c9dbc8e
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-01-19 13:47:12 -0600

    Fix $(DATA)
    Use EXTENSION_VERSION_FILES instead of depending on what's on the filesystem.

--- pgxntool/base.mk
+++ pgxntool/base.mk
@@ -42,7 +42,7 @@ endef
 $(foreach ext,$(EXTENSIONS),$(eval $(call extension--version_rule,$(ext))))
 #$(foreach ext,$(EXTENSIONS),$(info $(call extension--version_rule,$(ext))))
 
-DATA         = $(filter-out $(wildcard sql/*-*-*.sql),$(wildcard sql/*.sql))
+DATA         = $(EXTENSION_VERSION_FILES)
 DOCS         = $(wildcard doc/*.asc)
 ifeq ($(strip $(DOCS)),)
 DOCS =# Set to NUL so PGXS doesn't puke
@@ -71,7 +71,7 @@ GE91		 = $(call test, $(MAJORVER), -ge, 91)
 ifeq ($(GE91),yes)
 all: $(EXTENSION_VERSION_FILES)
 
-DATA = $(wildcard sql/*--*.sql)
+#DATA = $(wildcard sql/*--*.sql)
 endif
 
 PGXS := $(shell $(PG_CONFIG) --pgxs)
